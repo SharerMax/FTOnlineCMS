fdbd7abf78a1190d23ab12e712f9b34408016062
Max Zhao <mdcw1103@gmail.com>
2024-03-24 20:04:25 +0800

    feat: add crawler task log

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import 'reflect-metadata'
 import process from 'node:process'
 import { Provider } from './repository/entry/provider'
 import { Crawler } from './repository/crawler/crawler'
+import { CrawlerTask } from './repository/entry/crawler-task'
 import { AppDataSource } from '@/repository/data-source'
 import 'dotenv/config'
 
@@ -10,9 +11,18 @@ try {
   const providerRepository = AppDataSource.getRepository(Provider)
   const providers = await providerRepository.find({ where: { enable: true } })
   if (providers.length > 0) {
+    const crawlerTaskRepository = AppDataSource.getRepository(CrawlerTask)
     for (const provider of providers) {
       const crawler = new Crawler(provider.id, provider.apiUrl)
-      await crawler.run()
+      const startTime = new Date()
+      const crawlerTask = await crawlerTaskRepository.save({ providerId: provider.id, status: 1, startTime, endTime: startTime })
+      try {
+        await crawler.run()
+        await crawlerTaskRepository.update({ id: crawlerTask.id }, { endTime: new Date(), status: 2 })
+      }
+      catch (error) {
+        await crawlerTaskRepository.update({ id: crawlerTask.id }, { endTime: new Date(), status: 3 })
+      }
     }
   }
 }
