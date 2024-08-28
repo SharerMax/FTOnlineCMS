c18d7e22d21bc900477d407d69d5116acb2a830d
Max Zhao <mdcw1103@gmail.com>
2024-08-29 00:50:51 +0800

    chore: update deps

--- src/env.d.ts
+++ src/env.d.ts
@@ -2,5 +2,6 @@ namespace NodeJS {
   interface ProcessEnv {
     MAX_PAGE: string
     NODE_ENV: string
+    DEBUG: string
   }
 }
--- tsup.config.ts
+++ tsup.config.ts
@@ -10,7 +10,7 @@ export default defineConfig((options) => {
       dts: false,
       format: ['esm'],
       watch: true,
-      onSuccess: 'DEBUG=* node dist/app.js --enable-source-maps',
+      onSuccess: 'node dist/app.js --enable-source-maps',
     }
   }
   return {
